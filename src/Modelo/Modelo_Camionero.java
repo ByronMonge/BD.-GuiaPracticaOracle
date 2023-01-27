2a4131743f3ab2685289276300c08f82d7251f68
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-27 10:30:43 -0500

    Arregle la clase modelo_camionero

--- src/Modelo/Modelo_Camionero.java
+++ src/Modelo/Modelo_Camionero.java
@@ -14,35 +14,47 @@ public class Modelo_Camionero extends Camionero {
     public Modelo_Camionero() {
     }
 
-    public Modelo_Camionero(int codigoCam, String dni, String nombre, String apellido, String telefono, String direccion, String poblacion, double salario) {
-        super(codigoCam, dni, nombre, apellido, telefono, direccion, poblacion, salario);
-    }
-
     public List<Camionero> listaPersonasTabla() {
         try {
-            //Me retorna un "List" de "persona"
-            List<Camionero> lista = new ArrayList<>();
+            List<Camionero> listacam = new ArrayList<>();
 
-            String sql = "select nombre from persona";
+            String sql = "select * from persona,camionero where per_codigo = cam_percodigo;";
 
             ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
-            byte[] bytea;
 
             //Pasar de "ResultSet" a "List"
             while (rs.next()) {
-                //Crear las instancias de las personas
-                Camionero persona = new Camionero();
+
+                Camionero camionero = new Camionero();
 
                 //Todo lo que haga en la sentencia define como voy a extraer los datos
-                persona.setNombre(rs.getString("nombre"));
+                camionero.setCodigo(rs.getInt(1));
+                camionero.setDni(rs.getString(2));
+                camionero.setPrinombre(rs.getString(3));
+                camionero.setSegnombre(rs.getString(4));
+                camionero.setApellidopat(rs.getString(5));
+                camionero.setApellidomat(rs.getString(6));
+                camionero.setDireccion(rs.getString(7));
+                camionero.setTelefono(rs.getString(8));
+                camionero.setEmail(rs.getString(9));
+                camionero.setFechanac(rs.getDate(10));
+                camionero.setEdad(rs.getInt(11));
+                camionero.setGenero(rs.getString(12));
+                camionero.setCodigoCam(rs.getInt(13));
+                camionero.setCodigo(rs.getInt(14)); //Otra vez es el codigo de persona
+                camionero.setSalario(rs.getDouble(15));
+                camionero.setTipolicencia(rs.getString(16));
+                camionero.setAniosexperiencia(rs.getInt(17));
+                camionero.setEstado(rs.getString(18));
+                
 
-                lista.add(persona); //Agrego los datos a la lista
+                listacam.add(camionero); //Agrego los datos a la lista
             }
 
             //Cierro la conexion a la BD
             rs.close();
             //Retorno la lista
-            return lista;
+            return listacam;
 
         } catch (SQLException ex) {
             Logger.getLogger(Modelo_Camionero.class.getName()).log(Level.SEVERE, null, ex);
