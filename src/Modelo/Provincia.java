7cba97ecb0e0743786052ee065bfb8488b157d32
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-26 20:39:28 -0500

    Arregle la clase provincia

--- src/Modelo/Provincia.java
+++ src/Modelo/Provincia.java
@@ -5,13 +5,17 @@ public class Provincia {
     
     private int codigoPro;
     private String nombre;
+    private String region;
+    private int numcanton;
 
     public Provincia() {
     }
 
-    public Provincia(int codigoPro, String nombre) {
+    public Provincia(int codigoPro, String nombre, String region, int numcanton) {
         this.codigoPro = codigoPro;
         this.nombre = nombre;
+        this.region = region;
+        this.numcanton = numcanton;
     }
 
     public int getCodigoPro() {
@@ -28,5 +32,21 @@ public class Provincia {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public int getNumcanton() {
+        return numcanton;
+    }
+
+    public void setNumcanton(int numcanton) {
+        this.numcanton = numcanton;
     }
 }
