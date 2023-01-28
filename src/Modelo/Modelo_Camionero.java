8b1b24c8ed8da8688115135ffee3da1210d8ecec
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-27 23:41:16 -0500

    Modelo camionero

--- src/Modelo/Modelo_Camionero.java
+++ src/Modelo/Modelo_Camionero.java
@@ -1,9 +1,9 @@
 package Modelo;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,63 +12,38 @@ public class Modelo_Camionero extends Camionero {
 
     ConectOC conoc = new ConectOC();
 
+    Modelo_Persona mipersona = new Modelo_Persona(); //Creo un objeto de modelo_persona para usar sus metodos
+
     public Modelo_Camionero() {
     }
 
-    public List<Camionero> listaCamioneros() {
-        try {
-            List<Camionero> listacam = new ArrayList<>();
+    public Modelo_Camionero(int codigoCam, double salario, String tipolicencia, int aniosexperiencia, String estado) {
+        super(codigoCam, salario, tipolicencia, aniosexperiencia, estado);
+    }
 
-            String sql = "select * from persona,camionero where per_codigo = cam_percodigo;";
+    public Modelo_Camionero(int codigoCam, double salario, String tipolicencia, int aniosexperiencia, String estado, int codigo, String dni, String prinombre, String segnombre, String apellidopat, String apellidomat, String direccion, String telefono, String email, Date fechanac, int edad, String genero) {
+        super(codigoCam, salario, tipolicencia, aniosexperiencia, estado, codigo, dni, prinombre, segnombre, apellidopat, apellidomat, direccion, telefono, email, fechanac, edad, genero);
+    }
 
-            ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
+    public boolean crearCamionero() {
 
-            //Pasar de "ResultSet" a "List"
-            while (rs.next()) {
+        if (mipersona.crearPersona()) {
+            //String sql = "insert into camionero (cam_percodigo, cam_salario, cam_tipolicencia, cam_aniosexperi, cam_estado) values (" + mipersona.traerCodigoDePersona() + "," + getSalario() + ",'" + getTipolicencia() + "'," + getAniosexperiencia() + ",'A')";
 
-                Camionero camionero = new Camionero();
-
-                //Todo lo que haga en la sentencia define como voy a extraer los datos
-                camionero.setCodigo(rs.getInt(1));
-                camionero.setDni(rs.getString(2));
-                camionero.setPrinombre(rs.getString(3));
-                camionero.setSegnombre(rs.getString(4));
-                camionero.setApellidopat(rs.getString(5));
-                camionero.setApellidomat(rs.getString(6));
-                camionero.setDireccion(rs.getString(7));
-                camionero.setTelefono(rs.getString(8));
-                camionero.setEmail(rs.getString(9));
-                camionero.setFechanac(rs.getDate(10));
-                camionero.setEdad(rs.getInt(11));
-                camionero.setGenero(rs.getString(12));
-                camionero.setCodigoCam(rs.getInt(13));
-                camionero.setCodigo(rs.getInt(14)); //Otra vez es el codigo de persona
-                camionero.setSalario(rs.getDouble(15));
-                camionero.setTipolicencia(rs.getString(16));
-                camionero.setAniosexperiencia(rs.getInt(17));
-                camionero.setEstado(rs.getString(18));
-
-                listacam.add(camionero); //Agrego los datos a la lista
-            }
-
-            //Cierro la conexion a la BD
-            rs.close();
-            //Retorno la lista
-            return listacam;
-
-        } catch (SQLException ex) {
-            Logger.getLogger(Modelo_Camionero.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
+            String sql = "insert into camionero (cam_percodigo, cam_salario, cam_tipolicencia, cam_aniosexperi, cam_estado) values (4,400,'C',1,'A')";
+            return conoc.accion(sql);
+        } else {
+            return false;
         }
     }
 
-    public List<Camionero> listaPersonasTabla() { //SIRVE
+    public List<Camionero> listaPersonasTabla() {
         try {
             //Me retorna un "List" de "persona"
             List<Camionero> lista = new ArrayList<>();
 
             //String sql = "select per_codigo,per_prinombre,per_apellidopat,per_edad,per_genero,per_telefono from persona";SENTENCIA QUE SIRVE
-            String sql = "select per_codigo,per_prinombre,per_apellidopat,per_edad,per_genero,per_telefono, cam_salario from persona p, camionero c where p.per_codigo = c.cam_percodigo";
+            String sql = "select per_dni,per_prinombre,per_apellidopat,per_edad,per_genero,per_telefono, cam_salario from persona p, camionero c where p.per_codigo = c.cam_percodigo";
 
             ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
 
@@ -78,7 +53,7 @@ public class Modelo_Camionero extends Camionero {
                 Camionero cam = new Camionero();
 
                 //Todo lo que haga en la sentencia define como voy a extraer los datos
-                cam.setCodigo(rs.getInt("per_codigo"));
+                cam.setDni(rs.getString("per_dni"));
                 cam.setPrinombre(rs.getString("per_prinombre"));
                 cam.setApellidopat(rs.getString("per_apellidopat"));
                 cam.setEdad(rs.getInt("per_edad"));
