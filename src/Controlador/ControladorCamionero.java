c96013386e90da9467cd071fdcd615695f51b4be
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-27 20:12:31 -0500

    controlador camionero incompleto

--- src/Controlador/ControladorCamionero.java
+++ src/Controlador/ControladorCamionero.java
@@ -5,6 +5,7 @@ import Modelo.Modelo_Camionero;
 import Vista.VistaCamionero;
 import java.util.List;
 import javax.swing.table.DefaultTableModel;
+import javax.xml.ws.Holder;
 
 public class ControladorCamionero {
 
@@ -15,11 +16,12 @@ public class ControladorCamionero {
         this.modelo = modelo;
         this.vista = vista;
         vista.setVisible(true);
+        cargarTabla2();
     }
 
     public void iniciarControl() {
         vista.getBtncrear().addActionListener(l -> abrirDialogCrear());
-        vista.getBtnactualizar().addActionListener(l -> cargarTabla());
+        vista.getBtnactualizar().addActionListener(l -> cargarTabla2());
     }
 
     public void abrirDialogCrear() {
@@ -29,16 +31,36 @@ public class ControladorCamionero {
         vista.getjDlgCamionero().setTitle("Crear nueva persona");
         vista.getjDlgCamionero().setVisible(true);
     }
-    
-    public void cargarTabla() {
+
+    public void cargarTabla2() {
+        vista.getTablacamioneros().setRowHeight(25);
+        DefaultTableModel estructuraTabla = (DefaultTableModel) vista.getTablacamioneros().getModel();
+        estructuraTabla.setRowCount(0);
+
+        List<Camionero> listap = modelo.listaPersonasTabla2();
+
+        Holder<Integer> i = new Holder<>(0);
+
+        listap.stream().forEach(persona -> {
+            
+                    estructuraTabla.addRow(new Object[8]);
+
+                    vista.getTablacamioneros().setValueAt(persona.getCodigo(), i.value, 0);
+                    vista.getTablacamioneros().setValueAt(persona.getPrinombre(), i.value, 1);
+
+                    i.value++;
+                });
+    }
+
+    /*public void cargarTabla1() {//SIRVE
         DefaultTableModel tabla = (DefaultTableModel) vista.getTablacamioneros().getModel();
         tabla.setNumRows(0);
 
         //System.out.println("llena datos");
-        List<Camionero> cams = modelo.listaCamioneros();
+        List<Camionero> cams = modelo.listaPersonasTabla();
         cams.stream().forEach(p -> {
-            String[] datos = {p.getDni(),p.getPrinombre(), p.getApellidopat(), String.valueOf(p.getEdad()), p.getGenero(), p.getTelefono(), String.valueOf(p.getSalario())};
+            String[] datos = {p.getPrinombre()};
             tabla.addRow(datos);
         });
-    }
+    }*/
 }
