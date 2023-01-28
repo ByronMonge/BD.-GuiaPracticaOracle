acf24e136e5b3a2d5db931931dcc7e4b82747e46
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-28 12:14:14 -0500

    Modifique los constructores

--- src/Modelo/Modelo_Persona.java
+++ src/Modelo/Modelo_Persona.java
@@ -13,15 +13,8 @@ public class Modelo_Persona extends Persona {
     public Modelo_Persona() {
     }
 
-    public Modelo_Persona(int codigo, String dni, String prinombre, String segnombre, String apellidopat, String apellidomat, String direccion, String telefono, String email, Date fechanac, int edad, String genero) {
+    public Modelo_Persona(int codigo, String dni, String prinombre, String segnombre, String apellidopat, String apellidomat, String direccion, String telefono, String email, String fechanac, int edad, String genero) {
         super(codigo, dni, prinombre, segnombre, apellidopat, apellidomat, direccion, telefono, email, fechanac, edad, genero);
-    }
-
-    public boolean crearPersona() {
-        //String sql = "insert into persona (per_dni, per_prinombre, per_segnombre, per_apellidopat, per_apellidomat, per_direccion, per_telefono, per_email, per_fechanac, per_edad, per_genero) values ('" + getDni() + "','" + getPrinombre() + "','" + getSegnombre() + "','" + getApellidopat() + "','" + getApellidomat() + "','" + getDireccion() + "','" + getTelefono() + "','" + getEmail() + "'," + getFechanac() + "," + getEdad() + ",'" + getGenero() + "')";
-
-        String sql = "insert into persona (per_dni, per_prinombre, per_segnombre, per_apellidopat, per_apellidomat, per_direccion, per_telefono, per_email, per_fechanac, per_edad, per_genero) values ('4444444444','Pedro','Samuel','Aldaz','Coronel','Cuenca','0967342523','victor46@gmail.com','21/08/2003',19,'H')";
-        return conoc.accion(sql);
     }
 
     public int traerCodigoDePersona() {
