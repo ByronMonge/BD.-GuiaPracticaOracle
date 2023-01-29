26c1fc981071adc2f187a8305c072ecec224287f
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-28 22:03:23 -0500

    Crud terminado

--- src/Modelo/Modelo_Persona.java
+++ src/Modelo/Modelo_Persona.java
@@ -40,12 +40,12 @@ public class Modelo_Persona extends Persona {
 
         return codigo;
     }
-    
+
     public int traerCodigoDePersonaModificar(String cedula) { //Trae el codigo de la persona dependiendo el numero de cedula pasado como parametro
         int codigo = 0;
         try {
 
-            String sql = "select per_codigo from persona where per_dni = '"+cedula+"'";
+            String sql = "select per_codigo from persona where per_dni = '" + cedula + "'";
 
             ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
 
@@ -64,5 +64,30 @@ public class Modelo_Persona extends Persona {
         }
 
         return codigo;
+    }
+
+    public int validarRepetidos(String cedula) { //Metodo que sirve para validar la cantidad de cedulas existentes en la BD
+        int cantidad = 0;
+        try {
+
+            String sql = "select COUNT(*) from persona where per_dni='"+cedula+"'";
+
+            ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
+
+            //Pasar de "ResultSet" a "List"
+            while (rs.next()) {
+                cantidad = rs.getInt("COUNT(*)"); //Trae la cantidad de dni repetidos
+
+            }
+
+            //Cierro la conexion a la BD
+            rs.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Modelo_Camionero.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+
+        return cantidad;
     }
 }
