f18e0fdba19a26a198841fadb9498fb4b9fd00a2
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-30 16:42:59 -0500

    Vista camion

--- src/Vista/VistaCamion.java
+++ src/Vista/VistaCamion.java
@@ -193,7 +193,7 @@ public class VistaCamion extends javax.swing.JInternalFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 264, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 270, Short.MAX_VALUE))
         );
 
         pack();
