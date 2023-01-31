bec697319c0e69ac66204d67119cd8581c29d8f0
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-30 20:14:24 -0500

    modelo conduce

--- src/Modelo/Modelo_Conduce.java
+++ src/Modelo/Modelo_Conduce.java
@@ -1,6 +1,20 @@
 
 package Modelo;
 
-public class Modelo_Conduce {
+public class Modelo_Conduce extends Conduce{
     
+    ConectOC conoc = new ConectOC();
+
+    public Modelo_Conduce() {
+    }
+
+    public Modelo_Conduce(int codigoCon, String fechaSalida, String fechaLlegada, int codigoCam, int codigoCmi) {
+        super(codigoCon, fechaSalida, fechaLlegada, codigoCam, codigoCmi);
+    }
+    
+    public boolean crearConduccion() {
+
+        String sql = "insert into conduce (con_codcamionero, con_codcamion, con_fechainicio, con_fechafin) values (" + getCodigoCam() + "," + getCodigoCmi() + ",'" + getFechaSalida() + "','" + getFechaLlegada() + "')";
+        return conoc.accion(sql);
+    }
 }
