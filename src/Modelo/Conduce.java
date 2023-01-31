bb2a240ed8d587c63a0553b8f4771e7d19fa3309
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-01-30 20:13:58 -0500

    conduce

--- src/Modelo/Conduce.java
+++ src/Modelo/Conduce.java
@@ -1,19 +1,17 @@
 package Modelo;
 
-import java.util.Date;
-
 public class Conduce {
 
     private int codigoCon;
-    private Date fechaSalida;
-    private Date fechaLlegada;
+    private String fechaSalida;
+    private String fechaLlegada;
     private int codigoCam;
     private int codigoCmi;
 
     public Conduce() {
     }
 
-    public Conduce(int codigoCon, Date fechaSalida, Date fechaLlegada, int codigoCam, int codigoCmi) {
+    public Conduce(int codigoCon, String fechaSalida, String fechaLlegada, int codigoCam, int codigoCmi) {
         this.codigoCon = codigoCon;
         this.fechaSalida = fechaSalida;
         this.fechaLlegada = fechaLlegada;
@@ -29,19 +27,19 @@ public class Conduce {
         this.codigoCon = codigoCon;
     }
 
-    public Date getFechaSalida() {
+    public String getFechaSalida() {
         return fechaSalida;
     }
 
-    public void setFechaSalida(Date fechaSalida) {
+    public void setFechaSalida(String fechaSalida) {
         this.fechaSalida = fechaSalida;
     }
 
-    public Date getFechaLlegada() {
+    public String getFechaLlegada() {
         return fechaLlegada;
     }
 
-    public void setFechaLlegada(Date fechaLlegada) {
+    public void setFechaLlegada(String fechaLlegada) {
         this.fechaLlegada = fechaLlegada;
     }
 
