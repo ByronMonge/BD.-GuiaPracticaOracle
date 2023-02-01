79a18415cd10e492ef95fca5fe217e30f248f596
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-02-01 09:00:27 -0500

    ya

--- src/Modelo/Modelo_Destinatario.java
+++ src/Modelo/Modelo_Destinatario.java
@@ -1,5 +1,5 @@
-
 package Modelo;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,8 +7,8 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+public class Modelo_Destinatario extends Destinatario {
 
-public class Modelo_Destinatario extends Destinatario{
     ConectOC conoc = new ConectOC();
 
     public Modelo_Destinatario() {
@@ -21,7 +21,7 @@ public class Modelo_Destinatario extends Destinatario{
     public Modelo_Destinatario(int codigodes, String codpostal, String infocomplement, String calleprinc, String callesecun, int codigo, String dni, String prinombre, String segnombre, String apellidopat, String apellidomat, String direccion, String telefono, String email, String fechanac, int edad, String genero) {
         super(codigodes, codpostal, infocomplement, calleprinc, callesecun, codigo, dni, prinombre, segnombre, apellidopat, apellidomat, direccion, telefono, email, fechanac, edad, genero);
     }
-    
+
     public boolean crearPersona() {
 
         String sql = "insert into persona (per_dni, per_prinombre, per_segnombre, per_apellidopat, per_apellidomat, per_direccion, per_telefono, per_email, per_fechanac, per_edad, per_genero) values ('" + getDni() + "','" + getPrinombre() + "','" + getSegnombre() + "','" + getApellidopat() + "','" + getApellidomat() + "','" + getDireccion() + "','" + getTelefono() + "','" + getEmail() + "','" + getFechanac() + "'," + getEdad() + ",'" + getGenero() + "')";
@@ -34,29 +34,22 @@ public class Modelo_Destinatario extends Destinatario{
 
         if (crearPersona()) { //Si la persona ya existe entonces puedo crear un destinatario
 
-            String sql = "insert into destinatario (des_percodigo, des_codpostal, des_infocomplement, des_calleprinc, des_callesecun) values (" + mipersona.traerCodigoDePersonaCrear() + ",'" + getCodpostal() + "','" + getInfocomplement() + "','"+getCalleprinc()+"','"+getCallesecun()+"')";
+            String sql = "insert into destinatario (des_percodigo, des_codpostal, des_infocomplement, des_calleprinc, des_callesecun) values (" + mipersona.traerCodigoDePersonaCrear() + ",'" + getCodpostal() + "','" + getInfocomplement() + "','" + getCalleprinc() + "','" + getCallesecun() + "')";
             return conoc.accion(sql);
         } else {
             return false;
         }
     }
-    
+
     public boolean modificarPersonaYDestinatario() { //Modificar la instancia en la BD
         Modelo_Persona mipersona = new Modelo_Persona(); //Creo un objeto de la clase Modelo_Persona 
 
         String sqlP = "UPDATE persona set per_prinombre = '" + getPrinombre() + "',per_segnombre = '" + getSegnombre() + "', per_apellidopat = '" + getApellidopat() + "', per_apellidomat = '" + getApellidomat() + "', per_direccion = '" + getDireccion() + "', per_telefono = '" + getTelefono() + "', per_email = '" + getEmail() + "', per_fechanac = '" + getFechanac() + "', per_edad = " + getEdad() + ", per_genero = '" + getGenero() + "' where per_dni = '" + getDni() + "'"; //Modifica los datos en la tabla persona
 
-        String sqlC = "UPDATE destinatario SET des_codpostal = '" + getCodpostal() + "',des_infocomplement = '" + getInfocomplement() + "', des_calleprinc = '" + getCalleprinc() + "', des_callesecun = '" + getCallesecun() +  "' WHERE des_percodigo = '" + mipersona.traerCodigoDePersonaModificar(getDni()) + "'"; //Modifica los datos de la tabla camionero
+        String sqlC = "UPDATE destinatario SET des_codpostal = '" + getCodpostal() + "',des_infocomplement = '" + getInfocomplement() + "', des_calleprinc = '" + getCalleprinc() + "', des_callesecun = '" + getCallesecun() + "' WHERE des_percodigo = '" + mipersona.traerCodigoDePersonaModificar(getDni()) + "'"; //Modifica los datos de la tabla camionero
         return conoc.accion(sqlP) && conoc.accion(sqlC);
     }
 
-    public boolean eliminarDestinatario(String cedula) { //Modificar el estado del destinatario
-        Modelo_Persona mipersona = new Modelo_Persona(); //Creo un objeto de la clase Modelo_Persona 
-
-        String sqlC = "DELETE destinatario WHERE des_percodigo = '" + mipersona.traerCodigoDePersonaModificar(cedula) + "'";
-        return conoc.accion(sqlC);
-    }
-    
     public List<Destinatario> listaDestinatariosTabla() {
         try {
             //Me retorna un "List" de "persona"
