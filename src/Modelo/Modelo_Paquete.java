136b772175bdc2377306d04331107e97664eca9d
ByronMonge <byron.monge.est@tecazuay.edu.ec>
2023-02-01 00:03:15 -0500

    ESTOY USANDO

--- src/Modelo/Modelo_Paquete.java
+++ src/Modelo/Modelo_Paquete.java
@@ -75,4 +75,42 @@ public class Modelo_Paquete extends Paquete {
         }
     }
 
+    public List<Paquete> buscarPaquetes(int codigo) {
+        try {
+            //Me retorna un "List" de "persona"
+            List<Paquete> lista = new ArrayList<>();
+
+            String sql = "select * from paquete where paq_codigo like '"+codigo+"%'";
+
+            ResultSet rs = conoc.consulta(sql); //La consulta nos devuelve un "ResultSet"
+
+            //Pasar de "ResultSet" a "List"
+            while (rs.next()) {
+                //Crear las instancias de las personas
+                Paquete paq = new Paquete();
+
+                //Todo lo que haga en la sentencia define como voy a extraer los datos
+                paq.setCodigoPaq(rs.getInt("paq_codigo"));
+                paq.setCodcliente(rs.getInt("paq_codcliente"));
+                paq.setCodprovincia(rs.getInt("paq_codprovincia"));
+                paq.setCoddestina(rs.getInt("paq_coddestina"));
+                paq.setCodcamionero(rs.getInt("paq_codcamionero"));
+                paq.setPeso(rs.getDouble("paq_peso"));
+                paq.setFecharegistro(rs.getString("paq_fecharegistro"));
+                paq.setLlegadaprogra(rs.getString("paq_llegadaprogra"));
+                paq.setSalidaprogra(rs.getString("paq_salidaproga"));
+
+                lista.add(paq); //Agrego los datos a la lista
+            }
+
+            //Cierro la conexion a la BD
+            rs.close();
+            //Retorno la lista
+            return lista;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Modelo_Paquete.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
 }
